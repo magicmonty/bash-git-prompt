f2a38d66eff603a5b827c752e6aa243ed11da34d
Mariusz Smykuła <mariuszs@gmail.com>
2013-05-30 22:17:57 +0200

    Rename, more compatibility with original version

--- gitprompt.fish
+++ gitprompt.fish
@@ -13,38 +13,38 @@ set Blue (set_color blue)           # Blue
 set WHITE (set_color white)
 
 # Bold
-set BGreen (set_color green)        # Green
+set BGreen (set_color -o green)        # Green
 
 # High Intensty
-set IBlack (set_color black)        # Black
+set IBlack (set_color -o black)        # Black
 
 # Bold High Intensty
-set Magenta (set_color purple)      # Purple
+set Magenta (set_color -o purple)      # Purple
 
 # Various variables you might want for your PS1 prompt instead
 set Time (date +%X)
 
 # Default values for the appearance of the prompt. Configure at will.
-set GIT_PROMPT_PREFIX "("
-set GIT_PROMPT_SUFFIX ")"
+set GIT_PROMPT_PREFIX "["
+set GIT_PROMPT_SUFFIX "]"
 set GIT_PROMPT_SEPARATOR "|"
 set GIT_PROMPT_BRANCH "$Magenta"
-set GIT_PROMPT_STAGED "$Red}●"
-set GIT_PROMPT_CONFLICTS "$Red✖"
-set GIT_PROMPT_CHANGED "$Blue✚"
+set GIT_PROMPT_STAGED "$Red}● "
+set GIT_PROMPT_CONFLICTS "$Red✖ "
+set GIT_PROMPT_CHANGED "$Blue✚ "
 set GIT_PROMPT_REMOTE " "
 set GIT_PROMPT_UNTRACKED "…"
 set GIT_PROMPT_CLEAN "$BGreen✔"
 
 #Not applied two lines conention from https://github.com/magicmonty/bash-git-prompt/pull/5/files
 
-set PROMPT_END " % "
+set PROMPT_END " \n$WHITE$Time$ResetColor \$ "
 
 function fish_prompt
     set -e __CURRENT_GIT_STATUS
     set gitstatus "$__GIT_PROMPT_DIR/gitstatus.py"
 
-    set PROMPT_START "$IBlack$Time $ResetColor$Yellow"
+    set PROMPT_START "$Yellow"
 
     set _GIT_STATUS (python $gitstatus)
     set __CURRENT_GIT_STATUS $_GIT_STATUS
@@ -54,6 +54,10 @@ function fish_prompt
 
     if not test "0" -eq $__CURRENT_GIT_STATUS_PARAM_COUNT
         set GIT_REMOTE "$__CURRENT_GIT_STATUS[2]"
+        echo "-$GIT_REMOTE-"
+        if [ "." = "$GIT_REMOTE" ]
+            set -e GIT_REMOTE
+        end
         set GIT_STAGED $__CURRENT_GIT_STATUS[3]
         set GIT_CONFLICTS $__CURRENT_GIT_STATUS[4]
         set GIT_CHANGED $__CURRENT_GIT_STATUS[5]
@@ -70,23 +74,23 @@ function fish_prompt
 
         set STATUS "$STATUS$GIT_PROMPT_SEPARATOR"
 
-        if test $GIT_STAGED -ne "0"
+        if [ $GIT_STAGED != "0" ]
             set STATUS "$STATUS$GIT_PROMPT_STAGED$GIT_STAGED$ResetColor"
         end
 
-        if test $GIT_CONFLICTS -ne "0"
+        if [ $GIT_CONFLICTS != "0" ]
             set STATUS "$STATUS$GIT_PROMPT_CONFLICTS$GIT_CONFLICTS$ResetColor"
         end
 
-        if  test $GIT_CHANGED -ne "0"
+        if [ $GIT_CHANGED != "0" ]
             set STATUS "$STATUS$GIT_PROMPT_CHANGED$GIT_CHANGED$ResetColor"
         end
 
-        if test  "$GIT_UNTRACKED" -ne "0"
+        if [ "$GIT_UNTRACKED" != "0" ]
             set STATUS "$STATUS$GIT_PROMPT_UNTRACKED$GIT_UNTRACKED$ResetColor"
         end
 
-        if test "$GIT_CLEAN" -eq "1"
+        if [ "$GIT_CLEAN" = "1" ]
             set STATUS "$STATUS$GIT_PROMPT_CLEAN"
         end
 
