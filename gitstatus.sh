ea2da1ea6fb0de25bd00e6ed9b21e1bd522d85fe
Victor Seva <vseva@sipwise.com>
2014-01-30 15:39:40 +0100

    fix branch name.
    branches can have '/'. So, just remove the 'refs/heads' prefix.

--- gitstatus.sh
+++ gitstatus.sh
@@ -16,7 +16,7 @@ gitsym=`git symbolic-ref HEAD`
 case "$gitsym" in fatal*) exit 0 ;; esac
 
 # the current branch is the tail end of the symbolic reference
-branch="${gitsym##*/}"    # get the basename after "refs/head/"
+branch="${gitsym##refs/heads/}"    # get the basename after "refs/heads/"
 
 tmp="/tmp/$$-gitstatus.out"
 trap "rm -f \"$tmp\"" EXIT
