4ecaa1e645c5e426fe8c4825eed2ee6d689f2f3b
Alan Stebbens <aks@stebbens.org>
2014-08-28 00:20:00 -0700

    aks: make the prompt vars all global

--- git-prompt-colors.sh
+++ git-prompt-colors.sh
@@ -1,3 +1,17 @@
+# These are the color definitions used by gitprompt.sh
+
+declare -g GIT_PROMPT_PREFIX
+declare -g GIT_PROMPT_SUFFIX
+declare -g GIT_PROMPT_SEPARATOR
+declare -g GIT_PROMPT_BRANCH
+declare -g GIT_PROMPT_STAGED
+declare -g GIT_PROMPT_CONFLICTS
+declare -g GIT_PROMPT_CHANGED
+declare -g GIT_PROMPT_REMOTE
+declare -g GIT_PROMPT_UNTRACKED
+declare -g GIT_PROMPT_STASHED
+declare -g GIT_PROMPT_CLEAN
+
 local Time12a="\$(date +%H:%M)"
 local PathShort="\w"
 
@@ -8,12 +22,12 @@ GIT_PROMPT_SEPARATOR="|"              # separates each item
 
 GIT_PROMPT_BRANCH="${Magenta}"        # the git branch that is active in the current directory
 GIT_PROMPT_STAGED="${Red}●"           # the number of staged files/directories
-GIT_PROMPT_CONFLICTS="${Red}✖"        # the number of files in conflict
-GIT_PROMPT_CHANGED="${Blue}✚"         # the number of changed files
+GIT_PROMPT_CONFLICTS="${Red}✖ "       # the number of files in conflict
+GIT_PROMPT_CHANGED="${Blue}✚ "        # the number of changed files
 
 GIT_PROMPT_REMOTE=" "                 # the remote branch name (if any) and the symbols for ahead and behind
 GIT_PROMPT_UNTRACKED="${Cyan}…"       # the number of untracked files/dirs
-GIT_PROMPT_STASHED="${BoldBlue}⚑"     # the number of stashed files/dir
+GIT_PROMPT_STASHED="${BoldBlue}⚑ "    # the number of stashed files/dir
 GIT_PROMPT_CLEAN="${BoldGreen}✔"      # a colored flag indicating a "clean" repo
 
 GIT_PROMPT_COMMAND_OK="${Green}✔ "    # indicator if the last command returned with an exit code of 0
