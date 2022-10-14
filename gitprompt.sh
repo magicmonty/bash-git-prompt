352aaf0273441dd2f46fb9e7c0b4bee194407ab5
Günther Grill <guenhter@users.noreply.github.com>
2022-10-14 09:27:54 +0200

    Revert "Replace while read loop with a readarray"
    This reverts commit 5da0a40f6dc871c651ef49b6952b5e285cafe856.

--- gitprompt.sh
+++ gitprompt.sh
@@ -501,7 +501,7 @@ function updatePrompt() {
   export GIT_INDEX_FILE="${GIT_INDEX_PRIVATE}"
 
   local -a git_status_fields
-  readarray -t git_status_fields < <("${__GIT_STATUS_CMD}" 2>/dev/null)
+  while IFS=$'\n' read -r line; do git_status_fields+=("${line}"); done < <("${__GIT_STATUS_CMD}" 2>/dev/null)
 
   export GIT_BRANCH=$(replaceSymbols "${git_status_fields[0]}")
   local GIT_REMOTE="$(replaceSymbols "${git_status_fields[1]}")"
